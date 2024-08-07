0f87dc9f51303cbe9991987d8db490133fb39eb2
Mayuresh Singh <mayuresh@Mayureshs-Laptop.local>
2024-08-07 15:05:07 +0530

    First Repo

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask # type: ignore
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    application.run(host='0.0.0.0', port=8080)
